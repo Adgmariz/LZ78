53b55b11aeeb8c7b5d5521f5bf5d347c328a2dd0
Alexis Mariz <adgmariz@gmail.com>
2023-05-01 18:49:28 -0300

    Programa funcionando 100%

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         nome_saida = ""
         if len(sys.argv) == 5:
             if sys.arg[3] == "-o":
-                nome_saida = sys.argv[4].rstrip(".txt") + ".z78"
+                nome_saida = sys.argv[4]
             else:
                 print("Parâmetros inválidos")
         else:
@@ -24,8 +24,8 @@ def main():
     elif(sys.argv[1] == "-x"):
         nome_saida = ""
         if len(sys.argv) == 5:
-            if sys.arg[3] == "-o":
-                nome_saida = sys.argv[4].rstrip(".z78") + ".txt"
+            if sys.argv[3] == "-o":
+                nome_saida = sys.argv[4]
             else:
                 print("Parâmetros inválidos")
         else:
@@ -40,14 +40,11 @@ def main():
                 if len(token) == 0:
                     break
                 if len(token) < 4:
-                    print("ERRO BINÁRIO, len:"+str(len(token)))
-                    print(token)
-                    print("-")
+                    break
                 caractere = token[0]
                 index = (token[3] << 16) | (token[2] << 8) | (token[1])
                 indices.append(index)
                 caracteres.append(caractere)
-        nome_saida = "saida.txt"
         with open(nome_saida, "wb") as arquivo:
             for i in range(1,len(indices)):
                 if indices[i] == 0:
@@ -65,10 +62,4 @@ def main():
 
     else:
         print("Parâmetros inválidos.")
-    
-    #Temp:
-
-    #trie = Trie(texto)
-    #trie.comprimir()
-    #trie.descomprimir()
 main()
