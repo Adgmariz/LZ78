ad4b3b2e3d455f34cd090576ae4d16757e038112
Alexis Mariz <adgmariz@gmail.com>
2023-05-02 18:02:10 -0300

    Upload dos casos de teste e do relatório em pdf.

--- Trie.py
+++ Trie.py
@@ -6,7 +6,6 @@ class Trie:
         self.index = 1
         self.texto = texto
         self.no_atual = self.raiz
-        #self.comprimido = ""
         self.tokens = np.array([],dtype=np.uint32)
 
     def comprimir(self):
@@ -14,7 +13,6 @@ class Trie:
             filho = self.no_atual.getFilhoPorCaractere(caractere)
             if not filho:
                 self.no_atual.adiciona_filho(self.index,caractere)
-                #self.comprimido += str(self.no_atual.index) + chr(caractere)
                 binario = np.zeros(1, dtype=np.uint32)
                 binario[0] |= (self.no_atual.index << 8)
                 binario[0] |= (caractere)
